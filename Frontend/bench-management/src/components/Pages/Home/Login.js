c69bb7232253b3f2831e115ccb01788ecbc4402f
dhruvBansal0809 <dhruv.bansal@accolitedigital.com>
2023-08-10 13:28:21 +0530

    changes to the cover page, fixed forced referesh

--- Frontend/bench-management/src/components/Pages/Home/Login.js
+++ Frontend/bench-management/src/components/Pages/Home/Login.js
@@ -8,11 +8,32 @@ import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const authData = useContext(AuthContext);
+  const google = window.google;
   const { REACT_APP_GOOGLE_CLIENT_ID, REACT_APP_URL } = process.env;
   const navigate = useNavigate();
+  
   function handleNotVerfied() {
     alert("Sorry you are not authorized to access!!");
   }
+
+  useEffect(() => {
+    (
+      authData.mfaEnabled === "0" ? (
+        navigate("/setup2fa", {replace:true})
+      )
+        : (
+          authData.otpVerify === false ? (
+            navigate("/verify2fa", {replace:true})
+          )
+            : (
+              authData.currentRole === "admin" ? (
+                navigate("/admin", {replace:true})
+              )
+                : navigate("/manager", {replace:true})
+            )
+        )
+    )
+  },[authData.isAuthentication]);
 
   function handleCallbackResponse(response) {
     // console.log(response.credential); jwt
@@ -23,17 +44,13 @@ export default function Login() {
       headers: { 'Content-Type': 'text/plain' }
     })
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         if (res.data === "not verified") handleNotVerfied();
         else {
           let data = res.data;
           authData.setAccessToken(data.substring(data.indexOf('+ ') + 2, data.length - 2));
-          // console.log(data.substring(data.indexOf('+ ') + 2, data.length - 2));
-          // authData.setAccessToken("rmcqlfy47ykj4byp7ksm8qgwuezy7w");
           authData.setCurrentRole(data.substring(0, data.indexOf(' ')));
-          // console.log(data.substring(0, data.indexOf(' ')));
           authData.setMfaEnabled(data[data.length - 1]);
-          // console.log(data[data.length-1]);
           if (authData.currentRole != "admin") {
             authData.setManagerId(data.substring(0, data.indexOf(' ')));
           }
@@ -41,54 +58,40 @@ export default function Login() {
         }
       });
   }
-
+  
   useEffect(() => {
-    /* global google */
-    const google = window.google;
     google?.accounts.id.initialize({
       client_id: `${REACT_APP_GOOGLE_CLIENT_ID}`, // add client id in .env file
+      ux_mode: "popup",
       callback: handleCallbackResponse
     });
-
+  },[]);
+    
+  useEffect(() => {
     google?.accounts.id.renderButton(
       document.getElementById("loginButton"),
-      { theme: "outline", size: "large", shape: "pill", width: "400", height: "300" }
+      { theme: "outline", size: "large", shape: "pill", width: "350px" }
     );
-  }, [authData.handleLogout]);
+    google?.accounts.id.prompt();
+  }, []);
 
   return (
     authData.isAuthentication === false ?
       (
-          <div className='loginContainer'>
-            <div>
-              <img className='logoContainer' src={logoImage} alt="accoliteLogo" />
-            </div>
-            <div className='loginPageContent'>
-              <h5 className='welcomeHeading'>Welcome To</h5>
-              <h1 className='projectHeading'>Bench Management System</h1>
-              <b>
-                <hr className="hr-text" data-content="One Tap Below to Sign-in" />
-              </b>
-              <div id='loginButton' ></div>
-            </div>
+        <div className='loginContainer'>
+          <div>
+            <img className='logoContainer' src={logoImage} alt="accoliteLogo" />
           </div>
+          <div className='loginPageContent'>
+            <h5 className='welcomeHeading'>Welcome To</h5>
+            <h1 className='projectHeading'>Bench Management System</h1>
+            <b>
+              <hr className="hr-text" data-content="One Tap Below to Sign-in" />
+            </b>
+            <div id="loginButton"></div>
+          </div>
+        </div>
       )
-      : (
-        authData.mfaEnabled === "0" ? (
-          navigate("/setup2fa", {replace : true})
-        )
-          : (
-            authData.otpVerify === false ? (
-              navigate("/verify2fa", {replace : true})
-            )
-              : (
-                authData.currentRole === "admin" ? (
-                  navigate("/admin", { replace: true })
-                )
-                  : navigate("/manager", { replace: true })
-              )
-          )
-
-      )
+      : null
   )
 }
